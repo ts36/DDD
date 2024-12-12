a36d8c27fa15560ec48ad590aac92eda1b9cb831
ts36 <brittany8936@gmail.com>
2024-12-12 21:25:49 +0800

    Update database.php

--- database.php
+++ database.php
@@ -1,9 +1,9 @@
 <?php
 // database.php
-$servername = "your_server_name";
-$username = "your_db_username";
-$password = "your_db_password";
-$dbname = "your_database_name";
+$servername = "qoo.database.windows.net";
+$username = "tsou36";
+$password = "Aa123456";
+$dbname = "DB";
 
 try {
     $conn = new PDO("sqlsrv:Server=$servername;Database=$dbname", $username, $password);
