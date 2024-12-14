e3fa92ed43ca1b262e822357c074c4cb56bbe61c
ts36 <brittany8936@gmail.com>
2024-12-14 22:21:07 +0800

    Update database.php

--- database.php
+++ database.php
@@ -8,6 +8,10 @@ $dbname = "DB";
 try {
     $conn = new PDO("sqlsrv:Server=$servername;Database=$dbname", $username, $password);
     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+    // 確保傳入的資料使用 UTF-8
+    $conn->exec("SET ANSI_NULLS ON");
+    $conn->exec("SET ANSI_WARNINGS ON");
 } catch (PDOException $e) {
     echo "連接失敗: " . $e->getMessage();
     die();
