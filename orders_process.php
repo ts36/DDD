24449e5202f9fded04ed2db5c03ccabd16b63220
ts36 <brittany8936@gmail.com>
2024-12-14 23:51:54 +0800

    Update orders_process.php

--- orders_process.php
+++ orders_process.php
@@ -4,7 +4,6 @@ include 'database.php';
 session_start();
 $user_id = $_SESSION['user_id'];
 
-$stmt = $conn->prepare("SELECT * FROM orders WHERE user_id = :user_id ORDER BY order_date DESC");
 $stmt->execute(['user_id' => $user_id]);
 $orders = $stmt->fetchAll();
 ?>
